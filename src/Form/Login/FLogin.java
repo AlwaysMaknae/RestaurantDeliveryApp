5f3a865d1f631c59f37a4d27968f062f9c9bcc4b
Prouddom <domenicospada@hotmail.com>
2020-01-30 11:13:06 -0500

    Start

--- src/Form/Login/FLogin.java
+++ src/Form/Login/FLogin.java
@@ -1,15 +1,22 @@
 package Form.Login;
 
-import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
-public class FLogin extends FLoginView implements ActionListener{
-
+public class FLogin extends FLoginView{
+	
 	public FLogin(){
-		
+		BTNConnect.addActionListener(this);
 		System.out.println("Form Controller");
 		
-		
+		TFUsername.SetMask("^(.+)@(.+)$");
 	}
 	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		//Submit
+		System.out.println(TFUsername.GetContent());
+		
+	}
+
 	
 }
