e0bf8fe4d993b19eddba1827e37b2e5f85b4d0b7
Prouddom <domenicospada@hotmail.com>
2020-02-04 10:56:48 -0500

    Added Accept/View/Done Delivery Guy Account Form (View/Controller)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 import Form.Delivery.FAddDeliveryGuy;
 import Form.Delivery.FDeleteDeliveryGuy;
 import Form.Delivery.FEditDeliveryGuy;
+import Form.DeliveryGuy.FAcceptDelivery;
+import Form.DeliveryGuy.FDoneDelivery;
+import Form.DeliveryGuy.FViewDelivery;
 import Form.Login.FLogin;
 import Form.Menu.FAddMenu;
 import Form.Menu.FDeleteMenu;
@@ -17,18 +20,51 @@ public class Main {
 	public static void main(String[] args) {
 
 		new DBConnecter();
+		/*
+		 * LOGIN & CREATE/EDIT/DELETE ACCOUNT
+		 */
+		
 		//new FLogin();
 		//new FAddClient();
+		
+		/*
+		 * RESTAURANT FORM
+		 */
+		
 		//new FAddRestaurant();
 		//new FEditRestaurant();
 		//new FDeleteRestaurant();
 		//new FAddRestaurantMenu();
+		
+		/*
+		 * MENU FORM
+		 */
+		
 		//new FAddMenu();
 		//new FEditMenu();
 		//new FDeleteMenu();
+		
+		/*
+		 * DELIVERY FORM
+		 * ***NOTE*** FORM.DELIVERY JAVA CLASSES ARE CALLED "DELIVERYGUY" WHILE
+		 * FORM.DELIVERYGUY JAVA CLASSES ARE CALLED "DELIVERY". THIS WAS A BIG UH-OH
+		 * MADE BY DOM. PLEASE FORGIVE HIM.
+		 */
+		
 		//new FAddDeliveryGuy();
 		//new FEditDeliveryGuy();
 		//new FDeleteDeliveryGuy();
+		
+		/*
+		 * DELIVERYGUY ACCOUNT FORM
+		 * ***NOTE*** FORM.DELIVERYGUY JAVA CLASSES ARE CALLED "DELIVER" WHILE
+		 * FORM.DELIVERY JAVA CLASSES ARE CALLED "DELIVERYGUY". THIS WAS A BIG UH-OH
+		 * MADE BY DOM. PLEASE FORGIVE HIM.
+		 */
+		
+		//new FViewDelivery();
+		//new FAcceptDelivery();
+		//new FDoneDelivery();
 		
 
 		DBConnecter.OpenConnection();
