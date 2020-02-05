f445f322f10bffbf53cb8e7eb24a35f5158050d8
jose <jose.19315@gmail.com>
2020-02-04 23:50:11 -0500

    Added all stored procs, missing overload procedures(Im saving them for later after we have the basic program

--- db/restaurantdelivery (1).sql
+++ db/restaurantdelivery (1).sql
@@ -80,7 +80,7 @@ DROP TABLE IF EXISTS `items`;
 CREATE TABLE IF NOT EXISTS `items` (
   `item_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
   `item_dish` varchar(35) NOT NULL,
-  `item_price` int(11) NOT NULL,
+  `item_price` float NOT NULL,
   `restaurant_id` int(10) UNSIGNED NOT NULL,
   `status` tinyint(4) NOT NULL DEFAULT '1',
   PRIMARY KEY (`item_id`),
@@ -101,7 +101,7 @@ CREATE TABLE IF NOT EXISTS `orders` (
   `order_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `order_items` text NOT NULL,
   `order_delivery_time` time DEFAULT NULL,
-  `order_price` int(11) NOT NULL,
+  `order_price` float NOT NULL,
   `order_status` varchar(9) NOT NULL DEFAULT 'NOT READY',
   `restaurant_id` int(11) UNSIGNED NOT NULL,
   `deliverer_id` int(10) UNSIGNED NOT NULL,
@@ -190,6 +190,8 @@ COMMIT;
 -- ALL PROCEDUES ARE FROM HERE AND BELOW
 
 DELIMITER ☻
+
+-- USERS PROCEDURES
 CREATE PROCEDURE create_user(IN username VARCHAR(20), IN psswrd VARCHAR(50), IN access_lvl INT(11))
 BEGIN
 	INSERT INTO users(username, password, access_lvl) VALUES (username, psswrd, access_lvl);
@@ -216,3 +218,98 @@ BEGIN
     
 -- CALL delete_user(13);
 
+--   CLIENT PROCEDURES BELOW
+
+CREATE PROCEDURE create_client(IN client_username VARCHAR(20), IN client_password VARCHAR(50), IN client_last_name VARCHAR(50), IN client_first_name VARCHAR(50), IN client_address VARCHAR(50), IN client_email VARCHAR(64), IN client_number VARCHAR(12), IN status INT)
+BEGIN
+	INSERT INTO clients(client_username, client_password, client_last_name, client_first_name, client_address, client_email, client_number, status)
+		VALUES (client_username, client_password, client_last_name, client_first_name, client_address, client_email, client_number, status);
+    END☻
+    
+CREATE PROCEDURE update_client(IN client_id INT, IN client_username VARCHAR(20), IN client_password VARCHAR(50), IN client_last_name VARCHAR(50), IN client_first_name VARCHAR(50), IN client_address VARCHAR(50), IN client_email VARCHAR(64), IN client_number VARCHAR(12))
+BEGIN 
+	UPDATE clients SET clients.client_username=client_username, clients.client_password=client_password, clients.client_last_name=client_last_name, clients.client_first_name=client_first_name, clients.client_address=client_address, clients.client_email=client_email, clients.client_number=client_number
+		WHERE clients.client_id=client_id;
+    END☻
+    
+CREATE PROCEDURE delete_client(IN client_id INT)
+BEGIN
+	UPDATE clients SET clients.status=0 WHERE clients.client_id=client_id;
+    END☻
+
+-- RESTAURANT PROCEDURES BELOW
+CREATE PROCEDURE create_restaurant(IN restaurant_name VARCHAR(30), IN restaurant_address VARCHAR(50), IN restaurant_number VARCHAR(12), IN restaurant_hours VARCHAR(200), IN restaurant_areas VARCHAR(200), IN status INT)
+BEGIN
+	INSERT INTO restaurants(restaurant_name, restaurant_address, restaurant_number, restaurant_hours, restaurant_areas, status)
+		VALUES(restaurant_name, restaurant_address, restaurant_number, restaurant_hours, restaurant_areas, status);
+    END☻
+    
+CREATE PROCEDURE update_restaurant(IN restaurant_id INT, IN restaurant_name VARCHAR(30), IN restaurant_address VARCHAR(50), IN restaurant_number VARCHAR(12), IN restaurant_hours VARCHAR(200), IN restaurant_areas VARCHAR(200))
+BEGIN
+	UPDATE restaurants SET restaurants.restaurant_name=restaurant_name, restaurants.restaurant_address=restaurant_address, restaurants.restaurant_number=restaurant_number, restaurants.restaurant_hours=restaurant_hours, restaurants.restaurant_areas=restaurant_areas
+		WHERE restaurants.restaurant_id=restaurant_id;
+	END☻
+   
+CREATE PROCEDURE delete_restaurant(IN restaurant_id INT)
+BEGIN
+	UPDATE restaurants SET restaurants.status=0 WHERE restaurants.restaurant_id=restaurant_id;
+    END☻
+
+-- ITEMS PROCEDURE BELOW
+CREATE PROCEDURE create_item(IN item_dish VARCHAR(35), IN item_price FLOAT, IN restaurant_id INT, IN status INT)
+BEGIN
+	INSERT INTO items(item_dish, item_price, restaurant_id, status)
+		VALUES(item_dish, item_price, restaurant_id, status);
+	END☻
+    
+CREATE PROCEDURE update_item(IN item_id INT, IN item_dish VARCHAR(35), IN item_price FLOAT)
+BEGIN
+	UPDATE items SET items.item_dish=item_dish, items.item_price=item_price WHERE items.item_id=item_id;
+    END☻
+
+CREATE PROCEDURE delete_item(IN item_id INT)
+BEGIN
+	UPDATE items SET items.status=0 WHERE items.item_id=item_id;
+    END☻
+
+-- ORDERS PROCEDURE BELOW
+CREATE PROCEDURE create_order(IN order_address VARCHAR(200), IN order_postal_code VARCHAR(7), IN order_date DATETIME, IN order_items TEXT, IN order_delivery_time TIME,
+								IN order_price FLOAT, IN order_status VARCHAR(9), IN restaurant_id INT, IN deliverer_id INT, IN client_id INT, IN order_delivered TINYINT)
+BEGIN
+	INSERT INTO orders(order_address, order_postal_code, order_date, order_items, order_delivery_time, order_price, order_status, restaurant_id,
+						deliverer_id, client_id, order_delivered)
+		VALUES(order_address, order_postal_code, order_date, order_items, order_delivery_time, order_price, order_status, restaurant_id,
+						deliverer_id, client_id, order_delivered);
+	END☻
+    
+CREATE PROCEDURE update_order(IN order_id INT, IN order_address VARCHAR(200), IN order_post_code VARCHAR(7), IN order_items TEXT, IN order_delivery_time TIME,
+								IN deliverer_id INT, IN order_delivered INT)
+BEGIN
+	UPDATE orders SET orders.order_address=order_address, orders.order_post_code=order_post_code, orders.order_items=order_items, orders.order_delivery_time=order_delivery_time,
+						orders.deliverer_id=deliverer_id, orders.order_delivered=order_delivered
+		WHERE orders.order_id=order_id;
+	END☻
+
+
+CREATE PROCEDURE delete_order(IN order_id INT)
+BEGIN
+	UPDATE orders SET orders.order_delivered=1 WHERE orders.order_id=order_id;
+    END☻
+    
+-- DELIVERERS PROCEDURES BELOW
+CREATE PROCEDURE create_deliverer(IN deliverer_name VARCHAR(50), IN deliverer_number VARCHAR(12), IN deliverer_area VARCHAR(200), IN status INT)
+BEGIN
+	INSERT INTO deliverers(deliverer_name, deliverer_number, deliverer_area, status)
+		VALUES(deliverer_name, deliverer_number, deliverer_area, status);
+	END☻
+    
+CREATE PROCEDURE update_deliverer(IN deliverer_id INT, IN deliverer_name VARCHAR(50), IN deliverer_number VARCHAR(12), IN deliverer_area VARCHAR(200))
+BEGIN
+	UPDATE deliverers SET deliverers.deliverer_name=deliverer_name, deliverers.deliverer_number=deliverer_number, deliverers.deliverer_area=deliverer_area
+		WHERE deliverers.deliverer_id=deliverer_id;
+	END☻
+
+CREATE PROCEDURE delete_deliverer(IN deliverer_id INT)
+BEGIN
+	UPDATE deliverers SET deliverers.status=0 WHERE deliverers.deliverer_id=deliverer_id;
+    END☻
