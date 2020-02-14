fe6d5458c572a58375daec34b06993272f68f6e6
Jose <jose.19315@gmail.com>
2020-02-14 11:27:56 -0500

    creating order working, validation missing

--- src/Form/Client/FOrderFood.java
+++ src/Form/Client/FOrderFood.java
@@ -23,22 +23,36 @@ import utils.Navigator;
 
 public class FOrderFood extends FOrderFoodPage {
 	private ArrayList<ItemModel> MenuList;
+	private ArrayList<RestaurantModel> RestaurantList;
 	private String order_items;
 	private float sum;
+	private ClientModel current_client = new ClientModel(Session.GetUser().getId());
 
 	public FOrderFood() {
-
-		ArrayList<RestaurantModel> RestaurantList = DBRestaurant.getAllRestaurants();
-		ArrayList<Object> Restaurant = new ArrayList<Object>();
-
-		for (int i = 0; i < RestaurantList.size(); i++) {
-			Restaurant.add(RestaurantList.get(i).getName());
-		}
-
-		ArrayList<Object> Menu = new ArrayList<Object>();
+		BTNDelete.setEnabled(false);
+		
+		current_client.Read();
 		ArrayList<Object> Order = new ArrayList<Object>();
+		ArrayList<Object> Menu = new ArrayList<Object>();
 
-		ListPan.SetList(Restaurant);
+		TFPostalCode.SetMask("L#L");
+
+		BTNArea.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Menu.clear();
+				ListPan2.SetList(Menu);
+
+				RestaurantList = DBRestaurant.getRestaurantByArea(TFPostalCode.getText());
+				ArrayList<Object> Restaurant = new ArrayList<Object>();
+
+				for (int i = 0; i < RestaurantList.size(); i++) {
+					Restaurant.add(RestaurantList.get(i).getName());
+				}
+				ListPan.SetList(Restaurant);
+
+			}
+		});
 
 		BTNRestaurant.addActionListener(new ActionListener() {
 			@Override
@@ -51,10 +65,6 @@ public class FOrderFood extends FOrderFoodPage {
 				}
 
 				ListPan2.SetList(Menu);
-				
-
-				
-				
 
 			}
 		});
@@ -73,20 +83,18 @@ public class FOrderFood extends FOrderFoodPage {
 		BTNAdd.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				
-				if (TFQuantity==null || isNumeric(TFQuantity.getText())==false){
+
+				if (TFQuantity == null || isNumeric(TFQuantity.getText()) == false) {
 					System.out.println("yeet");
-				}else{
-					order_items = "(" + TFQuantity.getText() + "x)" + TFMeal.getText() + ": "
-							+ String.valueOf(Float.parseFloat(TFPrice.getText()) * Float.parseFloat(TFQuantity.getText()))
-							+ "$";
+				} else {
+					order_items = "(" + TFQuantity.getText() + "x)" + TFMeal.getText() + ": " + String.valueOf(
+							Float.parseFloat(TFPrice.getText()) * Float.parseFloat(TFQuantity.getText())) + "$";
 
 					Order.add(order_items);
 					ListPan3.SetList(Order);
 					sum += (Float.parseFloat(TFPrice.getText()) * Float.parseFloat(TFQuantity.getText()));
 					TFTotal.setText(String.valueOf(sum));
-					
+
 					System.out.println(Order);
 				}
 
@@ -116,8 +124,10 @@ public class FOrderFood extends FOrderFoodPage {
 				// FAlerts.Error("Selection Error", "Please select a Restaurant
 				// to order from.");
 				// }
-				if (MenuList.isEmpty()==false){
+				if (MenuList.isEmpty() == false) {
 					BTNRestaurant.setEnabled(false);
+					BTNArea.setEnabled(false);
+					BTNDelete.setEnabled(true);
 				}
 			}
 		});
@@ -125,14 +135,16 @@ public class FOrderFood extends FOrderFoodPage {
 		BTNOrder.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				ClientModel me = new ClientModel(Session.GetUser().getId());
-				me.Read();
-				
-				DBOrder.AddOrder(new OrderModel(me.getClient_address(), TFPostalCode.getText(), TFDeliveryTimeYMD.getText(), Order.toString(), TFHour.getText() + ":" + TFMinute.getText() + ":00", 
-						Float.parseFloat(TFPrice.getText()), "NOT READY", RestaurantList.get(ListPan.GetSelectedIndex()).getId(), me.getId()));
-				
-				FAlerts.Say("Order Deletion Cancelled", "Order was ordered in the correct order without any order from anyone successfully!");
-				Navigator.Dashboard(Me);
+
+				DBOrder.AddOrder(new OrderModel(current_client.getClient_address(), TFPostalCode.getText(),
+						TFDeliveryTimeYMD.getText(), Order.toString(),
+						TFHour.getText() + ":" + TFMinute.getText() + ":00", Float.parseFloat(TFPrice.getText()),
+						"NOT READY", RestaurantList.get(ListPan.GetSelectedIndex()).getId(), current_client.getId()));
+
+				// FAlerts.Say("Order Deletion Cancelled", "Order was ordered in
+				// the correct order without any order from anyone
+				// successfully!");
+				// Navigator.Dashboard(Me);
 			}
 		});
 
@@ -158,31 +170,37 @@ public class FOrderFood extends FOrderFoodPage {
 				// cancelled successfully!");
 				// }
 				// }
-				String selected = Order.get(ListPan3.GetSelectedIndex()).toString();
-				String x = Order.get(ListPan3.GetSelectedIndex()).toString().substring(selected.indexOf(": ") + 2, selected.indexOf("$"));
-				float selected_price = Float.parseFloat(x);
-				/*if (){
-					System.out.println();
-				}*/
-				Order.remove(ListPan3.GetSelectedIndex());
 
-				ListPan3.SetList(Order);
-				sum -= (selected_price);
-				TFTotal.setText(String.valueOf(new DecimalFormat("##.##").format(sum)));
+				if (Order.isEmpty() == false) {
+					String selected = Order.get(ListPan3.GetSelectedIndex()).toString();
+					String x = Order.get(ListPan3.GetSelectedIndex()).toString().substring(selected.indexOf(": ") + 2,
+							selected.indexOf("$"));
+					float selected_price = Float.parseFloat(x);
+
+					Order.remove(ListPan3.GetSelectedIndex());
+
+					ListPan3.SetList(Order);
+					sum -= (selected_price);
+					TFTotal.setText(String.valueOf(new DecimalFormat("##.##").format(sum)));
+				}
+				if (ListPan3.isEmpty()) {
+					BTNRestaurant.setEnabled(true);
+					BTNArea.setEnabled(true);
+					BTNDelete.setEnabled(false);
+				}
+
 			}
 		});
-		
+
 		BTNCancel.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
 
-				System.out.println("Redirect to the main page");
+				Navigator.Dashboard(Me);
 
 			}
 
 		});
-		
-		
 
 	}
 
